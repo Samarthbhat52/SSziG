1af76902daef886195cdb59159f02fa640176fe9
Samarth Bhat <samarthrbhat52@gmail.com>
2025-07-06 12:17:58 +0530

    v1 file to html converter
    minor error correction

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
+
+// TODO: Change this to stdout
 const print = std.debug.print;
+const path = std.fs.path;
 const Allocator = std.mem.Allocator;
 
 const clap = @import("clap");
@@ -10,6 +13,12 @@ const TokenType = @import("./lexer/token.zig").TokenType;
 const Html = @import("./nodeToHtml.zig").Html;
 const Parser = @import("./parser/parser.zig").Parser;
 
+const FSError = error{
+    NotMdFileError,
+    MissingExtError,
+    InvalidFormatError,
+};
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -18,9 +27,8 @@ pub fn main() !void {
     const params = comptime clap.parseParamsComptime(
         \\-h, --help             Display this help and exit.
         \\-v, --version          Display version and exit.
-        \\-f, --file             Markdown file path.
-        \\-d, --dir              Directory of markdown files (can be nested).
-        \\<str>
+        \\-f, --file <str>       Markdown file path.
+        \\-d, --dir  <str>       Directory of markdown files (can be nested).
         \\
     );
 
@@ -43,11 +51,18 @@ pub fn main() !void {
         return;
     }
 
+    if (res.args.file) |filename| {
+        try parseFile(filename);
+        return;
+    }
+
     try replFunction(allocator);
 }
 
 fn replFunction(alloc: Allocator) !void {
-    const stdin = std.io.getStdIn().reader();
+    const stdin_reader = std.io.getStdIn().reader();
+    var stdin_buffer = std.io.bufferedReader(stdin_reader);
+    const stdin = stdin_buffer.reader();
 
     print("\nSSziG v{s}\n", .{config.version});
     print("write in a markdown line and see the parsed output\n\n", .{});
@@ -84,4 +99,71 @@ fn replFunction(alloc: Allocator) !void {
             break;
         }
     }
+}
+
+fn parseFile(filepath: []const u8) !void {
+    const allocator = std.heap.page_allocator;
+
+    try sanitizeFilepath(filepath);
+
+    const dir = path.dirname(filepath) orelse ".";
+    const filename = path.basename(filepath);
+
+    // Read from file.
+    const cwd = std.fs.cwd();
+    const file = try cwd.openFile(filepath, .{ .mode = .read_only });
+    defer file.close();
+
+    const max_bytes = std.math.maxInt(usize);
+    const input = cwd.readFileAlloc(allocator, filepath, max_bytes) catch |err| {
+        // TODO: Handle this error better
+        print("Failed to read file: {}\n", .{err});
+        return err;
+    };
+
+    // Parse md to html.
+    var lex = Lexer.init(input);
+    var p = try Parser.init(allocator, &lex);
+    var document = try p.parse();
+    defer document.deinit();
+
+    var generator = Html.init(allocator);
+    const html = try generator.generateHtml(document);
+    defer allocator.free(html);
+
+    try createHtmlFile(allocator, dir, filename, html);
+}
+
+fn sanitizeFilepath(filepath: []const u8) FSError!void {
+    const ext = path.extension(filepath);
+
+    // check for proper markdown file.
+    if (ext.len == 0) return FSError.MissingExtError;
+    if (!std.mem.eql(u8, ext, ".md")) {
+        print("file format: {s}", .{ext});
+        return FSError.InvalidFormatError;
+    }
+}
+
+fn createHtmlFile(
+    allocator: Allocator,
+    dir: []const u8,
+    filename: []const u8,
+    content: []const u8,
+) !void {
+    const ext = ".md";
+    // Remove the 'md' extension.
+    const basename_no_ext = filename[0 .. filename.len - ext.len];
+
+    const html_filename = try std.mem.concat(allocator, u8, &.{ basename_no_ext, ".html" });
+    defer allocator.free(html_filename);
+
+    const html_path = try std.fs.path.join(allocator, &.{ dir, html_filename });
+    defer allocator.free(html_path);
+
+    // TODO: Change this to buffered writer.
+    var file = try std.fs.cwd().createFile(html_path, .{ .read = true, .truncate = true });
+    defer file.close();
+
+    try file.writeAll(content);
 }
